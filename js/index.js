e4fd1d0e97affaded22fc45c5739421e82f7ba24
CernaianuMihai <56340398+CernaianuMihai@users.noreply.github.com>
2019-10-23 17:09:55 +0300

    getName

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
-function loadDoc() {
+function getName() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
+            document.getElementById("root").innerHTML = JSON.parse(this.responseText).name;
         }
     };
 
-    xhr.open('GET', 'http://whoami/api/getname');
+    xhr.open('GET', 'http://whoami/api/getfullname');
     xhr.withCredentials = true;
     xhr.setRequestHeader('Content-Type', 'text/plain');
     xhr.send();
 }
+          
