698cef20e5c6abe9ddb9fa1c419e71e671e2bc74
CernaianuMihai <56340398+CernaianuMihai@users.noreply.github.com>
2019-10-24 14:55:18 +0300

    getName

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ function sendRequest(elementId, url, prop) {
     xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             document.getElementById(elementId).innerHTML = JSON.parse(this.responseText)[prop];
+            // console.log(JSON.parse(this.responseText));
         }
     };
     xhr.open('GET', url);
@@ -37,4 +38,39 @@ function getData() {
     getDepartment();
     getTitle();
     getPhysicalOffice();
+    getIdentity();
+}
+
+getData();
+
+function getIdentity() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let response = this.responseText;
+            let finalString = '';
+
+            let years = Number(JSON.parse(response)['years']);
+            if(years !== 0 && years > 1){
+                finalString += `${years} YEARS `;
+            }
+            else if (years !== 0){
+                finalString += `${years} YEAR `;
+            }
+
+            let months = Number(JSON.parse(response)['months']);
+            if(months !== 0 && months > 1){
+                finalString += `${months} MONTHS`;
+            }
+            else if (months !== 0){
+                finalString += `${months} MONTH`;
+            }
+
+            document.getElementById('duration').innerHTML = finalString;
+        }
+    };
+    xhr.open('GET', 'http://whoami/api/getemploymenttime');
+    xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'text/plain');
+    xhr.send();
 }
