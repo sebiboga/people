3d379206e03aaf35297f2d076bcad2d039b6798b
Magda Avram <magdahalus@gmail.com>
2019-10-25 13:06:25 +0300

    Ignore empty elem

--- js/listFillers.js
+++ js/listFillers.js
@@ -1,17 +1,18 @@
-function fill(fill_list,doc_id){
+function fill(fill_list, doc_id) {
     let html_elem = document.getElementById(`${doc_id}`);
 
-    for(let elem of fill_list){
-        console.log(elem);
-        let span = document.createElement("span");
-        span.innerHTML = elem;
-        span.className += " secondary skill uppercase border-secondary";
+    for (let elem of fill_list) {
+        if (elem !== "") {
+            let span = document.createElement("span");
+            span.innerHTML = elem;
+            span.className += " secondary skill uppercase border-secondary";
 
-        html_elem.appendChild(span);
+            html_elem.appendChild(span);
+        }
     }
 }
 
-function callFills(skills_list, tools_list){
+function callFills(skills_list, tools_list) {
     fill(skills_list,"skills-list");
     fill(tools_list,"tools-list");
 }
